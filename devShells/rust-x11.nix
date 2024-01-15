f774b88df290f361258cd56b5e405e45815fa614
L-Nafaryus <l.nafaryus@gmail.com>
2024-01-16 01:41:51 +0500

    devShells: rust-x11: update libraries

--- devShells/rust-x11.nix
+++ devShells/rust-x11.nix
@@ -1,10 +1,21 @@
 { pkgs, cranelib, ... }:
 cranelib.devShell {
-    packages = with pkgs; [ libGL xorg.libXi xorg.libX11 xorg.libXcursor lld libxkbcommon ];
+    packages = with pkgs; [ 
+        libGL 
+        xorg.libXi xorg.libX11 xorg.libXcursor xorg.libXrandr 
+        lld 
+        libxkbcommon
+        vulkan-loader
+    ];
 
     shellHook = ''
         export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
-            with pkgs; lib.makeLibraryPath [ libGL xorg.libX11 xorg.libXi xorg.libXcursor libxkbcommon ]
+            with pkgs; lib.makeLibraryPath [ 
+                libGL 
+                xorg.libX11 xorg.libXi xorg.libXcursor xorg.libXrandr 
+                libxkbcommon 
+                vulkan-loader 
+            ]
         }"
     '';
 }
