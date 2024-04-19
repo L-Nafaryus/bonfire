e4da1f6112fc7b03aa4b4f8548ddf5092ec93b57
L-Nafaryus <l.nafaryus@gmail.com>
2024-04-20 01:35:56 +0500

    update: apps: eliminate unnecessary apps

--- apps/default.nix
+++ apps/default.nix
@@ -5,13 +5,10 @@ let
 
 in forAllSystems(system: let 
         pkgs = nixpkgsFor.${system};
-        bpkgs = self.packages.${system};
-        blib = self.lib;
-    in {
-    
-    example = blib.mkApp { drv = bpkgs.example; name = "hello-nix"; };
-   
-    netgen = blib.mkApp { drv = bpkgs.netgen; };
 
-    spoof-dpi = blib.mkApp { drv = bpkgs.spoof-dpi; };
+        bonfire = self;
+        bonfire-lib = self.lib;
+        bonfire-pkgs = self.packages.${system};
+    in {
+ 
 })
